259dbdc47ebdce6f5cccc22409b056dcb1670ffa
Walid SADAOUI <walidsadaoui@hotmail.fr>
2025-02-18 15:18:13 +0100

    feat(drug) : add unit tests for generic cases

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -6,6 +6,16 @@ describe("Pharmacy", () => {
       [new Drug("test", 1, 2)],
     );
   });
+  it("should decrease benefit twice as fast after expiration", () => {
+    expect(new Pharmacy([new Drug("test", 0, 10)]).updateBenefitValue()).toEqual(
+      [new Drug("test", -1, 8)],
+    );
+  });
+  it("should keep benefit to 0 if negative", () => {
+    expect(new Pharmacy([new Drug("test", 2, 0)]).updateBenefitValue()).toEqual(
+      [new Drug("test", 1, 0)],
+    );
+  });
   it("should decrease the benefit and expiresIn for Dafalgan", () => {
     expect(new Pharmacy([new Drug("Dafalgan", 2, 3)]).updateBenefitValue()).toEqual(
       [new Drug("Dafalgan", 1, 1)],
